1f4e8740f91a9b75969ededad41aea18798d87fa
Ivan Mauricio Marin Lombo <black_death_metalero@outlook.com>
2023-05-01 15:40:35 -0500

    Inicio Automatica

--- BrunoUWP/Views/Frm_Configuracion.xaml.vb
+++ BrunoUWP/Views/Frm_Configuracion.xaml.vb
@@ -6,4 +6,42 @@
 Public NotInheritable Class Frm_Configuracion
     Inherits Page
 
+    Private Async Sub TglInicioAplicacion_Toggled(sender As Object, e As RoutedEventArgs)
+        Try
+            Dim startupTask As StartupTask = Await StartupTask.GetAsync("LaunchOnStartupTaskId")
+            Dim EstadoStartup = startupTask.State.ToString
+            If TglInicioAplicacion.IsOn = True Then
+                Select Case EstadoStartup
+                    Case "Disabled"
+                        Dim newState As StartupTaskState = Await startupTask.RequestEnableAsync()
+                        Debug.WriteLine("Request to enable startup, result = {0}", newState)
+                End Select
+            End If
+            If TglInicioAplicacion.IsOn = False Then
+                Select Case EstadoStartup
+                    Case "Enabled"
+                        startupTask.Disable()
+                End Select
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Async Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
+        Try
+            Dim startupTask As StartupTask = Await StartupTask.GetAsync("LaunchOnStartupTaskId")
+            Dim Retorno = startupTask.State.ToString
+            Select Case Retorno
+                Case "Disabled"
+                    TglInicioAplicacion.IsOn = False
+                Case "Enabled"
+                    TglInicioAplicacion.IsOn = True
+                Case Else
+                    TglInicioAplicacion.IsOn = False
+            End Select
+        Catch ex As Exception
+
+        End Try
+    End Sub
 End Class
