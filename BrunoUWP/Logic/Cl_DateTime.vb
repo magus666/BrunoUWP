8bc670b19b7830249380f423a8be0ee564cc53fd
Ivan Mauricio Marin Lombo <black_death_metalero@outlook.com>
2023-05-16 16:57:36 -0500

    Se implementa una interfaz

--- BrunoUWP/Logic/Cl_DateTime.vb
+++ BrunoUWP/Logic/Cl_DateTime.vb
@@ -1,11 +1,14 @@
 ﻿Imports System.Globalization
 
 Public Class Cl_DateTime
-
-    Public Function ObtenerHoraActual() As String
-        Dim FechaHoraActual As Date = Date.Now
-        Dim HoraActual As String = FechaHoraActual.ToString("hh:mm tt", CultureInfo.InvariantCulture)
-        Return HoraActual
+    Implements In_FechaHora
+    Public Function ObtenerHoraActual() As String Implements In_FechaHora.ObtenerHoraActual
+        Try
+            Dim FechaHoraActual As Date = Date.Now
+            Dim HoraActual As String = FechaHoraActual.ToString("hh:mm tt", CultureInfo.InvariantCulture)
+            Return HoraActual
+        Catch ex As Exception
+            Throw New Exception
+        End Try
     End Function
-
 End Class
---
+++ BrunoUWP/Logic/In_FechaHora.vb
@@ -0,0 +1,3 @@
+﻿Public Interface In_FechaHora
+    Function ObtenerHoraActual() As String
+End Interface
