6531220f52996858c80fddb89a6507091a19e045
Ivan Mauricio Marin Lombo <black_death_metalero@outlook.com>
2023-06-01 16:14:12 -0500

    Cambios Minimos

--- BrunoUWP/Views/Frm_Configuracion.xaml.vb
+++ BrunoUWP/Views/Frm_Configuracion.xaml.vb
@@ -1,7 +1,6 @@
 ﻿' La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
 Imports Microsoft.UI.Xaml.Controls
-Imports Windows.UI
 ''' <summary>
 ''' Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
 ''' </summary>
