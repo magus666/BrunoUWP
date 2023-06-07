3fde42b451e73235c9ac3fb8e69ea1c526b0436a
Ivan Mauricio Marin Lombo <black_death_metalero@outlook.com>
2023-06-07 08:22:09 -0500

    Filtro Mascota

--- BrunoUWP/Views/Frm_ConsultaMascota.xaml.vb
+++ BrunoUWP/Views/Frm_ConsultaMascota.xaml.vb
@@ -7,13 +7,39 @@ Imports Microsoft.Toolkit.Uwp.UI.Controls
 Public NotInheritable Class Frm_ConsultaMascota
     Inherits Page
     Dim GetMascota As New Cl_Mascota
+    Dim GetTipoMascota As New CL_TipoMascota
+    Dim GetRaza As New Cl_RazaMascota
+    Dim GetPersona As New Cl_Cliente
 
     Private Async Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
         Try
-            Dim Mascotas = Await GetMascota.ConsultaMascotas()
-            DtgMovimientoEquipos.ItemsSource = Mascotas
+            Await LlenarGrilla()
         Catch ex As Exception
 
         End Try
     End Sub
+
+    Public Async Function LlenarGrilla() As Task
+        Dim Mascota = Await GetMascota.ConsultaMascotas()
+        Dim TipoMascota = Await GetTipoMascota.ConsultarTipoMascota()
+        Dim Raza = Await GetRaza.ConsultarRazaMascota()
+        Dim Propietario = Await GetPersona.ConsultaCliente()
+
+        Dim RetornoMascota = (From Mas In Mascota
+                              Join Tmc In TipoMascota On
+                                   Mas.Tipo_Mascota Equals Tmc.Id_TipoMascota
+                              Join Rza In Raza On
+                                   Mas.Raza_Mascota Equals Rza.Id_Raza
+                              Join Ppo In Propietario On
+                                   Mas.Propietario_Mascota Equals Ppo.Id_Persona
+                              Select New With {Tmc.Nombre_TipoMascota,
+                                               Rza.Nombre_Raza,
+                                               Mas.Nombre_Mascota,
+                                               Mas.Edad_Mascota,
+                                               Ppo.NombreCompleto_Persona,
+                                               Mas.Observaciones_Mascota,
+                                               Mas.FechaRegistro_Mascota})
+
+        DtgMovimientoEquipos.ItemsSource = RetornoMascota
+    End Function
 End Class
