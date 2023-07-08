d213ddc10f3618600b9eb00f459c7d2104b9333b
Ivan Mauricio Marin Lombo <black_death_metalero@outlook.com>
2023-07-07 21:04:10 -0500

    ahhh

--- BrunoUWP/Views/Frm_CreaArticulo.xaml.vb
+++ BrunoUWP/Views/Frm_CreaArticulo.xaml.vb
@@ -1,19 +1,24 @@
 ﻿' La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
-Imports Windows.UI.Xaml.Controls.Maps
 ''' <summary>
 ''' Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
 ''' </summary>
 Public NotInheritable Class Frm_CreaArticulo
     Inherits Page
     Dim GetUtilitarios As New Cl_Utilitarios
+    Dim GetNotificaciones As New Cl_Notificaciones
     Dim GetArticulo As New Cl_Articulo
 
     Private Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
-        If String.IsNullOrEmpty(TxtValorArticulo.Text) Then
-            TxtValorArticulo.Text = "$"
-            TxtValorArticulo.SelectionStart = TxtValorArticulo.Text.Length
-        End If
+        Try
+            If String.IsNullOrEmpty(TxtValorArticulo.Text) Then
+                TxtValorArticulo.Text = "$"
+                TxtValorArticulo.SelectionStart = TxtValorArticulo.Text.Length
+            End If
+        Catch ex As Exception
+            GetNotificaciones.AlertaErrorInfoBar(InfAlerta, "Error", ex.Message)
+        End Try
+
     End Sub
 
     Private Sub TxtValorArticulo_BeforeTextChanging(sender As TextBox, args As TextBoxBeforeTextChangingEventArgs)
@@ -26,10 +31,10 @@ Public NotInheritable Class Frm_CreaArticulo
             If Await GetArticulo.InsertarArticulo(CodigoArticulo, TxtNombreArticulo.Text, TxtMarcaArticulo.Text,
                                                             TxtDescripcionArticulo.Text, TxtValorArticulo.Text,
                                                             NmbCantidadExistenciasArticulo.Text, Date.Now) = True Then
-                Dim Retorno = "Buena la Rata"
+                GetNotificaciones.AlertaErrorInfoBar(InfAlerta, "Exito", "El Articulo se ha creado con Exito.")
             End If
         Catch ex As Exception
-
+            GetNotificaciones.AlertaErrorInfoBar(InfAlerta, "Error", ex.Message)
         End Try
     End Sub
 End Class
