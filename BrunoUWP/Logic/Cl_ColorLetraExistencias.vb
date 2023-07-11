78e87a98aee54017e5e5f019fabdefdaeac292f8
Ivan Mauricio Marin Lombo <black_death_metalero@outlook.com>
2023-07-11 17:09:30 -0500

    Agregado Gridview para Articulos

---
+++ BrunoUWP/Logic/Cl_ColorLetraExistencias.vb
@@ -0,0 +1,20 @@
+﻿Imports Windows.UI
+
+Public Class Cl_ColorLetraExistencias
+    Implements IValueConverter
+
+    Public Function Convert(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.Convert
+        Dim Existencias As Integer = CType(value, Integer)
+        If Existencias >= 10 Then
+            Return New SolidColorBrush(Colors.Green)
+        ElseIf Existencias >= 6 Then
+            Return New SolidColorBrush(Colors.Yellow)
+        ElseIf Existencias >= 1 Then
+            Return New SolidColorBrush(Colors.Red)
+        End If
+    End Function
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.ConvertBack
+        Throw New NotImplementedException()
+    End Function
+End Class
--- BrunoUWP/Views/Frm_VentaArticulos.xaml.vb
+++ BrunoUWP/Views/Frm_VentaArticulos.xaml.vb
@@ -21,11 +21,7 @@ Public NotInheritable Class Frm_VentaArticulos
         Try
             CmbTipoArticulo.ItemsSource = Await GetMaestroArticulos.ConsultaMaestroArticulos()
             CmbTipoArticulo.DisplayMemberPath = "Nombre_MaestroArticulo"
-            CmbArticulo.IsEnabled = False
-
-            NbmCantidadVentaArticulo.IsEnabled = False
-            LblExistenciasArticulo.Visibility = Visibility.Collapsed
-            BtnVerificar.IsEnabled = False
+            GrdListaArticulos.Visibility = Visibility.Collapsed
         Catch ex As Exception
 
         End Try
@@ -38,108 +34,106 @@ Public NotInheritable Class Frm_VentaArticulos
             If CmbTipoArticulo.SelectedIndex = -1 Then
                 IdMaestroArticulo = 0
             Else
-                CmbArticulo.IsEnabled = True
+                GrdListaArticulos.Visibility = Visibility.Visible
                 IdMaestroArticulo = ItemSeleccionado.Id_MaestroArticulo
                 Dim ObtenerArticulos = Await GetArticulos.ConsultaArticulos()
                 Dim GetListaArticulos = (From Art In ObtenerArticulos
-                                         Where Art.Id_MaestroArticulo = IdMaestroArticulo)
-
-                CmbArticulo.ItemsSource = GetListaArticulos
-                CmbArticulo.DisplayMemberPath = "Nombre_Articulo"
+                                         Where Art.Id_MaestroArticulo = IdMaestroArticulo).ToList
+                GrvListadoArticulos.ItemsSource = GetListaArticulos
             End If
         Catch ex As Exception
 
         End Try
     End Sub
 
-    Private Async Sub CmbArticulo_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
-        Try
-            NbmCantidadVentaArticulo.IsEnabled = True
-            Dim ComboBoxSexo As ComboBox = CType(sender, ComboBox)
-            Dim ItemSeleccionado As ArticuloModel = CType(ComboBoxSexo.SelectedItem, ArticuloModel)
-            If CmbArticulo.SelectedIndex = -1 Then
-                IdArticulo = 0
-            Else
-                IdArticulo = ItemSeleccionado.Id_Articulo
-                Dim GetListaArticulos = Await GetArticulos.ConsultaArticulos()
-                Dim GetUnicoArticulo = (From Art In GetListaArticulos
-                                        Where Art.Id_Articulo = IdArticulo
-                                        Select Art.Nombre_Articulo,
-                                            Art.Cantidad_Articulo,
-                                            Art.Valor_Articulo).FirstOrDefault
-                Existencias = GetUnicoArticulo.Cantidad_Articulo
-                NombreArticulo = GetUnicoArticulo.Nombre_Articulo
-                ValorArticulo = GetUnicoArticulo.Valor_Articulo
-                LblExistenciasArticulo.Visibility = Visibility.Visible
-                LlenarExistencias()
-                BtnVerificar.IsEnabled = True
-            End If
-        Catch ex As Exception
+    'Private Async Sub CmbArticulo_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+    '    Try
+    '        NbmCantidadVentaArticulo.IsEnabled = True
+    '        Dim ComboBoxSexo As ComboBox = CType(sender, ComboBox)
+    '        Dim ItemSeleccionado As ArticuloModel = CType(ComboBoxSexo.SelectedItem, ArticuloModel)
+    '        If CmbArticulo.SelectedIndex = -1 Then
+    '            IdArticulo = 0
+    '        Else
+    '            IdArticulo = ItemSeleccionado.Id_Articulo
+    '            Dim GetListaArticulos = Await GetArticulos.ConsultaArticulos()
+    '            Dim GetUnicoArticulo = (From Art In GetListaArticulos
+    '                                    Where Art.Id_Articulo = IdArticulo
+    '                                    Select Art.Nombre_Articulo,
+    '                                        Art.Cantidad_Articulo,
+    '                                        Art.Valor_Articulo).FirstOrDefault
+    '            Existencias = GetUnicoArticulo.Cantidad_Articulo
+    '            NombreArticulo = GetUnicoArticulo.Nombre_Articulo
+    '            ValorArticulo = GetUnicoArticulo.Valor_Articulo
+    '            LblExistenciasArticulo.Visibility = Visibility.Visible
+    '            LlenarExistencias()
+    '            BtnVerificar.IsEnabled = True
+    '        End If
+    '    Catch ex As Exception
 
-        End Try
-    End Sub
+    '    End Try
+    'End Sub
 
-    Private Sub BtnVerificar_Click(sender As Object, e As RoutedEventArgs)
-        Try
-            Dim CantidadVenta As Integer = NbmCantidadVentaArticulo.Value
-            GrdResumenVenta.Visibility = Visibility.Visible
+    'Private Sub BtnVerificar_Click(sender As Object, e As RoutedEventArgs)
+    '    Try
+    '        Dim CantidadVenta As Integer = NbmCantidadVentaArticulo.Value
+    '        GrdResumenVenta.Visibility = Visibility.Visible
 
-            Dim ExistenciasFinales = Existencias - CantidadVenta
-            Existencias = ExistenciasFinales
-            LlenarExistencias()
+    '        Dim ExistenciasFinales = Existencias - CantidadVenta
+    '        Existencias = ExistenciasFinales
+    '        LlenarExistencias()
 
-            LblNombreArticulo.Text = NombreArticulo
-            LblCantidad.Text = CantidadVenta
-            LblValorUnitario.Text = ValorArticulo.ToString("C")
+    '        LblNombreArticulo.Text = NombreArticulo
+    '        LblCantidad.Text = CantidadVenta
+    '        LblValorUnitario.Text = ValorArticulo.ToString("C")
 
-            Dim ValorTotal As Double = CantidadVenta * ValorArticulo
-            LblValorTotal.Text = ValorTotal.ToString("C")
+    '        Dim ValorTotal As Double = CantidadVenta * ValorArticulo
+    '        LblValorTotal.Text = ValorTotal.ToString("C")
 
-        Catch ex As Exception
+    '    Catch ex As Exception
 
-        End Try
-    End Sub
+    '    End Try
+    'End Sub
 
-    Public Function LlenarExistencias() As Boolean
-        Try
-            NbmCantidadVentaArticulo.Maximum = Existencias
-            LblCountExistenciasArticulo.Text = Existencias
+    'Public Function LlenarExistencias() As Boolean
+    '    Try
+    '        NbmCantidadVentaArticulo.Maximum = Existencias
+    '        LblCountExistenciasArticulo.Text = Existencias
 
-            Select Case Existencias
-                Case >= 10
-                    LblCountExistenciasArticulo.Foreground = New SolidColorBrush(Colors.Green)
-                Case >= 5
-                    LblCountExistenciasArticulo.Foreground = New SolidColorBrush(Colors.Yellow)
-                Case >= 1
-                    LblCountExistenciasArticulo.Foreground = New SolidColorBrush(Colors.Red)
-            End Select
-            Return True
-        Catch ex As Exception
-            Throw New Exception(ex.Message)
-        End Try
-    End Function
+    '        Select Case Existencias
+    '            Case >= 10
+    '                LblCountExistenciasArticulo.Foreground = New SolidColorBrush(Colors.Green)
+    '            Case >= 5
+    '                LblCountExistenciasArticulo.Foreground = New SolidColorBrush(Colors.Yellow)
+    '            Case >= 1
+    '                LblCountExistenciasArticulo.Foreground = New SolidColorBrush(Colors.Red)
+    '        End Select
+    '        Return True
+    '    Catch ex As Exception
+    '        Throw New Exception(ex.Message)
+    '    End Try
+    'End Function
 
-    Public Function ValidaDatos() As Boolean
-        Try
-            If GetValidaciones.ValidaComboBoxVacio(CmbArticulo) = False Then
-                GetNotificaciones.ValidacionControlesTeachingTip(TctAlerta, "Alerta", "Seleccione un articulo Valido", CmbArticulo)
-                Return False
-            End If
-            Return True
-        Catch ex As Exception
-            Throw New Exception(ex.Message)
-        End Try
-    End Function
+    'Public Function ValidaDatos() As Boolean
+    '    Try
+    '        If GetValidaciones.ValidaComboBoxVacio(CmbArticulo) = False Then
+    '            GetNotificaciones.ValidacionControlesTeachingTip(TctAlerta, "Alerta", "Seleccione un articulo Valido", CmbArticulo)
+    '            Return False
+    '        End If
+    '        Return True
+    '    Catch ex As Exception
+    '        Throw New Exception(ex.Message)
+    '    End Try
+    'End Function
 
-    Private Async Sub BtnPagar_Click(sender As Object, e As RoutedEventArgs)
-        Try
-            Dim ValorPagar As Double
-            ValorPagar = 4200
-            Await GetUtilitarios.CrearContentDialogMetodoPago(ValorPagar)
-            GetUtilitarios.LimpiarControles(StpDatosArticulos)
-        Catch ex As Exception
+    'Private Async Sub BtnPagar_Click(sender As Object, e As RoutedEventArgs)
+    '    Try
+    '        Dim ValorPagar As Double
+    '        ValorPagar = 4200
+    '        Await GetUtilitarios.CrearContentDialogMetodoPago(ValorPagar)
+    '        GetUtilitarios.LimpiarControles(StpDatosArticulos)
+    '    Catch ex As Exception
 
-        End Try
+    '    End Try
 
-    End Sub
+    'End Sub
 End Class
