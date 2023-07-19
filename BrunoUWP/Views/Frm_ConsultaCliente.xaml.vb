ed90447c3ed5366161d639ff7c98a74dabc29302
Ivan Mauricio Marin Lombo <black_death_metalero@outlook.com>
2023-07-18 20:14:29 -0500

    Fix

--- BrunoUWP/Views/Frm_ConsultaCliente.xaml.vb
+++ BrunoUWP/Views/Frm_ConsultaCliente.xaml.vb
@@ -13,7 +13,7 @@ Public NotInheritable Class Frm_ConsultaCliente
 
     Private Async Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
         Try
-            ListadoFinalClientes = Await GetListaCliente()
+            ListadoFinalClientes = Await GetCliente.ConsultaCliente()
             If ListadoFinalClientes.Count = 0 Then
                 LblTituloCreacionCliente.Visibility = Visibility.Visible
                 LsvCliente.Visibility = Visibility.Collapsed
@@ -31,7 +31,7 @@ Public NotInheritable Class Frm_ConsultaCliente
 
     Private Async Sub AsbBusueda_TextChanged(sender As AutoSuggestBox, args As AutoSuggestBoxTextChangedEventArgs)
         Try
-            Dim GetListaClienteFiltro = Await GetListaCliente()
+            Dim GetListaClienteFiltro = Await GetCliente.ConsultaCliente()
 
             If AsbBusueda.Text.Count = 0 Then
                 LsvCliente.ItemsSource = ListadoFinalClientes
@@ -102,33 +102,6 @@ Public NotInheritable Class Frm_ConsultaCliente
         End Try
 
     End Sub
-
-    Public Async Function GetListaCliente() As Task(Of List(Of NewPersonaModel))
-        Try
-            Dim Clientes = Await GetCliente.ConsultaCliente()
-            Dim Sexo = Await GetSexo.ConsultaSexo()
-            Dim ListadoCliente = (From Cli In Clientes
-                                  Join Sex In Sexo On
-                                       Cli.Id_Sexo Equals Sex.Id_Sexo
-                                  Order By Cli.NombreCompleto_Persona
-                                  Select New NewPersonaModel With {.Id_Cliente = Cli.Id_Persona,
-                                                                   .Codigo_Cliente = Cli.Codigo_Cliente,
-                                                                    .Documento_Persona = Cli.Documento_Persona,
-                                                                    .Nombre_Persona = Cli.Nombre_Persona,
-                                                                    .Apellido_Persona = Cli.Apellido_Persona,
-                                                                    .NombreCompleto_Persona = Cli.NombreCompleto_Persona,
-                                                                    .Direccion_Persona = Cli.Direccion_Persona,
-                                                                    .Telefono_Persona = Cli.Telefono_Persona,
-                                                                    .Correo_Persona = Cli.Correo_Persona,
-                                                                    .Edad_Persona = Cli.Edad_Persona,
-                                                                    .Nombre_Sexo = Sex.Nombre_Sexo,
-                                                                    .FechaCreacion_Persona = Cli.FechaCreacion_Persona,
-                                                                    .NombreEstado_Cliente = If(Cli.Estado_Cliente, "Activo", "Inactivo")}).ToList
-            Return ListadoCliente
-        Catch ex As Exception
-            Throw New Exception(ex.Message)
-        End Try
-    End Function
 
     Private Sub RdbFiltroBusquedaCliente_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
         Try
