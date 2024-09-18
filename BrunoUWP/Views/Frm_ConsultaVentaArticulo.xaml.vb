6c3b0ce11cd49a66dbff640344698e65b9b03211
Ivan Mauricio Marin Lombo <black_death_metalero@outlook.com>
2024-09-18 01:51:15 -0500

    Se agrega ordenamiento a la tabla Venta articulo

--- BrunoUWP/Views/Frm_ConsultaVentaArticulo.xaml.vb
+++ BrunoUWP/Views/Frm_ConsultaVentaArticulo.xaml.vb
@@ -1,5 +1,7 @@
 ﻿' La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
+Imports Microsoft.Toolkit.Uwp.UI.Controls
+
 ''' <summary>
 ''' Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
 ''' </summary>
@@ -11,6 +13,7 @@ Public NotInheritable Class Frm_ConsultaVentaArticulo
     Dim GetTipoTransaccion As New Cl_TipoTransaccion
     Dim GetArticulo As New Cl_Articulo
     Dim GetCategoriaArticulo As New Cl_CategoriaArticulo
+    Private RetornoVentaArticulo As IEnumerable(Of Object)
 
     Private Async Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
         Try
@@ -19,23 +22,25 @@ Public NotInheritable Class Frm_ConsultaVentaArticulo
             Dim ObtenerCategoriaArticulo = Await GetCategoriaArticulo.ConsultaCategoriaArticulo
             Dim ObtenerTipoTransaccion = Await GetTipoTransaccion.ConsultaTipoTransaccion()
             Dim ObtenerArticulo = Await GetArticulo.IArticulo_ConsultaArticulos()
-            Dim RetornoVentaArticulo = (From Var In ObtenerVentaArticulo
-                                        Join Mpg In ObtenerMetodoPago On
+            Dim ListaArticulos = (From Var In ObtenerVentaArticulo
+                                  Join Mpg In ObtenerMetodoPago On
                                             Var.Id_MetodoPago Equals Mpg.Id_MetodoPago
-                                        Join Ttc In ObtenerTipoTransaccion On
+                                  Join Ttc In ObtenerTipoTransaccion On
                                             Var.Id_TipoTransaccion Equals Ttc.Id_TipoTransaccion
-                                        Join Art In ObtenerArticulo On
+                                  Join Art In ObtenerArticulo On
                                             Var.Id_Articulo Equals Art.Id_Articulo
-                                        Join Cat In ObtenerCategoriaArticulo On
+                                  Join Cat In ObtenerCategoriaArticulo On
                                             Art.Id_MaestroArticulo Equals Cat.Id_MaestroArticulo
-                                        Select New With {.Codigo = Var.Codigo_VentaArticulo,
+                                  Select New With {.Codigo = Var.Codigo_VentaArticulo,
                                                          .Fecha = Var.Fecha_VentaArticulo.ToShortDateString,
                                                          .TipoServicio = Ttc.Nombre_TipoTransaccion,
                                                          .Categoria = Cat.Nombre_MaestroArticulo,
                                                          .NombreArticulo = Art.Nombre_Articulo,
                                                          .Cantidad = Var.Cantidad_VentaArticulo,
                                                          .MetodoPago = Mpg.Nombre_MetodoPago,
-                                                         .ValorTotal = Var.Valor_VentaArticulo.ToString("c")})
+                                                         .ValorTotal = Var.Valor_VentaArticulo.ToString("c")}).ToList
+            RetornoVentaArticulo = (From item In ListaArticulos
+                                    Order By DateTime.Parse(item.Fecha) Descending).ToList()
             DtgVentaArticulo.ItemsSource = RetornoVentaArticulo
         Catch ex As Exception
             GetNotificaciones.AlertaErrorInfoBar(InfAlerta, "Error", ex.Message)
@@ -52,6 +57,47 @@ Public NotInheritable Class Frm_ConsultaVentaArticulo
                 GetNotificaciones.AlertaAdvertenciaInfoBar(InfAlerta, "Advertencia", "Se canceló la Exportacion a Excel")
                 PgrGeneraExcel.IsActive = False
             End If
+        Catch ex As Exception
+            GetNotificaciones.AlertaErrorInfoBar(InfAlerta, "Error", ex.Message)
+        End Try
+    End Sub
+
+    Private Sub DtgVentaArticulo_Sorting(sender As Object, e As Microsoft.Toolkit.Uwp.UI.Controls.DataGridColumnEventArgs)
+        Try
+            Dim Columna As String = e.Column.Tag.ToString
+            Select Case Columna
+                Case "Codigo"
+                    If e.Column.SortDirection Is Nothing OrElse e.Column.SortDirection = DataGridSortDirection.Descending Then
+                        DtgVentaArticulo.ItemsSource = (From item In RetornoVentaArticulo
+                                                        Order By item.Codigo Ascending)
+                        e.Column.SortDirection = DataGridSortDirection.Ascending
+                    Else
+                        DtgVentaArticulo.ItemsSource = (From item In RetornoVentaArticulo
+                                                        Order By item.Codigo Descending)
+                        e.Column.SortDirection = DataGridSortDirection.Descending
+                    End If
+                Case "FechaVenta"
+                    If e.Column.SortDirection Is Nothing OrElse e.Column.SortDirection = DataGridSortDirection.Descending Then
+                        DtgVentaArticulo.ItemsSource = (From item In RetornoVentaArticulo
+                                                        Order By DateTime.Parse(item.Fecha) Ascending)
+                        e.Column.SortDirection = DataGridSortDirection.Ascending
+                    Else
+                        DtgVentaArticulo.ItemsSource = (From item In RetornoVentaArticulo
+                                                        Order By DateTime.Parse(item.Fecha) Descending)
+                        e.Column.SortDirection = DataGridSortDirection.Descending
+                    End If
+                Case "NombreArticulo"
+                    If e.Column.SortDirection Is Nothing OrElse e.Column.SortDirection = DataGridSortDirection.Descending Then
+                        DtgVentaArticulo.ItemsSource = (From item In RetornoVentaArticulo
+                                                        Order By item.NombreArticulo Ascending)
+                        e.Column.SortDirection = DataGridSortDirection.Ascending
+                    Else
+                        DtgVentaArticulo.ItemsSource = (From item In RetornoVentaArticulo
+                                                        Order By item.NombreArticulo Descending)
+                        e.Column.SortDirection = DataGridSortDirection.Descending
+                    End If
+            End Select
+
         Catch ex As Exception
             GetNotificaciones.AlertaErrorInfoBar(InfAlerta, "Error", ex.Message)
         End Try
